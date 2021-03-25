1b2648031968bc511f8bed6534ced64fcc8a4d61
PC0283\user <last>
2021-03-25 16:35:07 +0800

    int

---
+++ src/main/java/com/last/ElasitcsearchApplication.java
@@ -0,0 +1,13 @@
+package com.last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasitcsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasitcsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/last/ElasitcsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.last;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasitcsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
