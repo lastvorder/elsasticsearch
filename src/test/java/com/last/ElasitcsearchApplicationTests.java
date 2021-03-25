5686695cb8b1475e3e65f628a8acc920ddb1cdd5
PC0283\user <last>
2021-03-25 17:42:34 +0800

    日志

--- src/test/java/com/last/ElasitcsearchApplicationTests.java
+++ src/test/java/com/last/ElasitcsearchApplicationTests.java
@@ -1,13 +1,23 @@
 package com.last;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ElasitcsearchApplicationTests {
 
     @Test
     void contextLoads() {
+
+        log.debug("debug日志");
+
+        log.info("info日志");
+
+        log.warn("warn日志");
+
+        log.error("error日志");
     }
 
 }
