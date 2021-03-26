ba958b60c2a3796367153268c3dc15209427bd1f
PC0283\user <last>
2021-03-26 16:43:00 +0800

    日志

---
+++ src/main/java/com/last/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.last.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author chenzihao
+ * @date 2021/03/26
+ */
+@Controller
+public class IndexController {
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "";
+    }
+}
