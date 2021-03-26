47d494cf8bb7be79573ea961a6c930d93c8f752e
PC0283\user <last>
2021-03-26 15:06:35 +0800

    日志

---
+++ src/main/java/com/last/util/ElasticSearchConst.java
@@ -0,0 +1,9 @@
+package com.last.util;
+
+/**
+ * @author chenzihao
+ * @date 2021/03/26
+ */
+public class ElasticSearchConst {
+    public static final String ES_INDEX = "user";
+}
--- src/test/java/com/last/ElasitcsearchApplicationTests.java
+++ src/test/java/com/last/ElasitcsearchApplicationTests.java
@@ -1,45 +1,48 @@
 package com.last;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.last.pojo.User;
+import com.last.util.ElasticSearchConst;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class ElasitcsearchApplicationTests {
 
-    private static ObjectMapper mapper = new ObjectMapper();
-
-    static {
-        // 对于空的对象转json的时候不抛出错误
-        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        // 允许属性名称没有引号
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-        // 允许单引号
-        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-        // 设置输入时忽略在json字符串中存在但在java对象实际没有的属性
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        // 设置输出时包含属性的风格
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    }
-
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Autowired
     @Qualifier("restHighLevelClient")
@@ -55,7 +58,7 @@ class ElasitcsearchApplicationTests {
         System.out.println(response);
     }
 
-    // 测试索引
+    // 测试索引是否存在
     @Test
     void testExistIndex() throws IOException {
         GetIndexRequest request = new GetIndexRequest("test1");
@@ -71,18 +74,114 @@ class ElasitcsearchApplicationTests {
         System.out.println(response.isAcknowledged());
     }
 
-    void testAddDocument() {
+    // 添加文档
+    @Test
+    void testAddDocument() throws IOException {
         // 创建对象
         User user = new User("陈子豪", 22);
         // 创建请求
         IndexRequest request = new IndexRequest("user");
         // 规则 put /user/_doc/1
-        request.id("1");
+        request.id("2");
         request.timeout(TimeValue.timeValueSeconds(1));
         // request.timeout("1s");
         // 将我们的数据放入请求 Json格式
-
-        // request.source(mapper.);
+        request.source(mapper.writeValueAsString(user), XContentType.JSON);
+        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+        System.out.println(response);
     }
 
+    // 获取文档，判断其是否存在 GET /index/_doc/1
+    @Test
+    void testIsExists() throws IOException {
+        GetRequest getRequest = new GetRequest("user", "1");
+        // 不获取 _source 的上下文
+        getRequest.fetchSourceContext(new FetchSourceContext(false));
+        getRequest.storedFields("_none_");
+        boolean exists = client.exists(getRequest, RequestOptions.DEFAULT);
+        System.out.println(exists);
+    }
+
+    // 获取文档
+    @Test
+    void testGetDocument() throws IOException {
+        GetRequest getRequest = new GetRequest("user", "1");
+        GetResponse getResponse = client.get(getRequest, RequestOptions.DEFAULT);
+        System.out.println(getResponse.getSourceAsString()); // 打印文档的内容
+        System.out.println(getResponse); // 返回的内容和命令行相同
+    }
+
+    // 更新文档信息
+    @Test
+    void testUpdateDocument() throws IOException {
+        UpdateRequest updateRequest = new UpdateRequest("user", "1");
+        updateRequest.timeout("1s");
+        User user = new User("鹿目圆", 14);
+        updateRequest.doc(mapper.writeValueAsString(user), XContentType.JSON);
+        UpdateResponse updateResponse = client.update(updateRequest, RequestOptions.DEFAULT);
+        System.out.println(updateResponse.status());
+    }
+
+    // 删除文档
+    @Test
+    void testDeleteDocument() throws IOException {
+        DeleteRequest deleteRequest = new DeleteRequest("user", "2");
+        deleteRequest.timeout("1s");
+        DeleteResponse deleteResponse = client.delete(deleteRequest, RequestOptions.DEFAULT);
+        System.out.println(deleteResponse.status());
+    }
+
+    // 特殊的，真的项目一般都会批量插入数据！
+    @Test
+    void testBulkRequest() throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout("1s");
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("user1", 21));
+        userList.add(new User("user2", 22));
+        userList.add(new User("user3", 23));
+        userList.add(new User("user4", 24));
+        userList.add(new User("user5", 25));
+        userList.add(new User("user6", 26));
+        userList.add(new User("user7", 27));
+        userList.add(new User("user8", 28));
+        userList.add(new User("user9", 29));
+        // ArrayList.forEach参数为一个Consumer<T>消费接口
+        userList.forEach(user -> {
+            try {
+                // 批量更新和批量删除，可修改对应的请求
+                bulkRequest.add(
+                        new IndexRequest("user").source(mapper.writeValueAsString(user), XContentType.JSON)
+                );
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        });
+        BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(bulkResponse.hasFailures()); // 是否失败，返回false表示成功
+    }
+
+    // 条件搜索文档
+    @Test
+    void testSearch() throws IOException {
+        SearchRequest searchRequest = new SearchRequest(ElasticSearchConst.ES_INDEX);
+        // 构建搜索条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        // 查询条件，我们可以使用QueryBuilders工具来实现
+        // QueryBuilders.termQuery 精确
+        // QueryBuilders.matchAllQuery() 匹配所有
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "user1");
+        // MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
+        sourceBuilder.query(termQueryBuilder);
+        sourceBuilder.from(0);
+        sourceBuilder.size(5);
+        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        searchRequest.source(sourceBuilder);
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        System.out.println(mapper.writeValueAsString(searchResponse.getHits()));
+        System.out.println("========================================================");
+        for (SearchHit documentFields : searchResponse.getHits().getHits()) {
+            System.out.println(documentFields.getSourceAsString());
+        }
+    }
 }
